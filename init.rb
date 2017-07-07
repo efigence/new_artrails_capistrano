e245ab54da4ce182e6af72df9c506fbaed3e88f1
Marcin Kalita <rubyconvict@gmail.com>
2017-07-07 09:48:13 +0200

    Remove files for script initialization

--- init.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require_relative 'env'
-require_relative 'lib/capistrano/artrails.rb'
-
-artrails = Capistrano::Artrails.new(ARGV.first)
-result = artrails.perform
-puts result
-exit 1 if result != 'ok'
