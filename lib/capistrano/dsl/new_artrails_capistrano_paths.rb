c363bd94630ee23ca66337f8db3fe177e17656ac
Marcin Kalita <rubyconvict@gmail.com>
2017-07-12 14:52:52 +0200

    Add log_dir_name setting

--- lib/capistrano/dsl/new_artrails_capistrano_paths.rb
+++ lib/capistrano/dsl/new_artrails_capistrano_paths.rb
@@ -9,6 +9,9 @@ module Capistrano
       def new_artrails_capistrano_front_remote_cache
         "shared/front-cached-copy-#{fetch(:local_user) || 'deploy'}"
       end
+      def new_artrails_capistrano_log_dir_name
+        File.basename(fetch(:deploy_to) || fetch(:application))
+      end
     end
   end
 end
